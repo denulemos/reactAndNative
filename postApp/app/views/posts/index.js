b3c1b382ffe69788fb36841e5dd3f9616f4413ed
Denisse Lemos <denu.a.lemos@gmail.com>
2020-09-17 14:17:32 -0300

    agregado useeffect

--- postApp/app/views/posts/index.js
+++ postApp/app/views/posts/index.js
@@ -9,16 +9,27 @@ import {styles} from './styles';
 
 const Posts = () => {
 
+    const [state, setState] = useState(0);
+
     //USO DEL USEEFFECT 
     //Recibe un parametro, pero si no recibe nada, por defecto, funciona como un ComponentDidMount
     useEffect(() => {
-        console.log('Hola! Actuo como un componentDidMount');
-    })
+        console.log('Update');
+    });
 
+    useEffect(() => {
+        console.log('Mount');
+    }, []);
+
+    useEffect(() => {
+        console.log('Update State');
+    },[state] )
+
+    
     return (
         <View style={styles.container}>
             <Button title="Usar" action = {() => {
-
+setState(1)
             }} />
         </View>
     );
